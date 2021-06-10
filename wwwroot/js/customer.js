727e769fc6251b596052973cf7335965dc98956a
Jasmine Randhawa <jasminerandhawa05@gmail.com>
2021-06-10 04:22:15 -0600

    added bootbox

--- wwwroot/js/customer.js
+++ wwwroot/js/customer.js
@@ -1,15 +1,22 @@
 ﻿
     $(document).ready(function () {
-        $("#customer").on("click", ".js-delete", function () {
-            var deleteButton = $(this);
-            if (confirm("Are you sure you want to delete this customer?")) {
-                $.ajax(
-                    {
-                        url: "/customer/delete/" + deleteButton.attr("data-customer-id"),
-                        success: function () {
-                            deleteButton.parents("tr").remove();
-                        }
-                    });
-            }
-        })
+        $("#customer").on("click", ".js-delete", function (result) {
+                var deleteButton = $(this);
+                //if (confirm("Are you sure you want to delete this customer?")) {
+
+            //}
+                bootbox.confirm("Are you sure you want to delete this customer?", function ()
+                {
+                    if (result) {
+                        $.ajax
+                            ({
+                                url: "/customer/delete/" + deleteButton.attr("data-customer-id"),
+                                success: function () {
+                                    deleteButton.parents("tr").remove();
+                                }
+
+                            });
+                    }
+                });
+            });
     });
--- wwwroot/js/movie.js
+++ wwwroot/js/movie.js
@@ -1,15 +1,20 @@
-﻿
-    $(document).ready(function () {
-        $("#movie").on("click", ".js-delete", function () {
-            var deleteButton = $(this);
-            if (confirm("Are you sure you want to delete this movie?")) {
-                $.ajax(
-                    {
+﻿$(document).ready(function () {
+    $("#movie").on("click", ".js-delete", function (result) {
+        var deleteButton = $(this);
+        //if (confirm("Are you sure you want to delete this movie?")) {
+
+        //}
+        bootbox.confirm("Are you sure you want to delete this movie?", function () {
+            if (result) {
+                $.ajax
+                    ({
                         url: "/movie/delete/" + deleteButton.attr("data-movie-id"),
                         success: function () {
                             deleteButton.parents("tr").remove();
                         }
+
                     });
             }
-        })
+            });
     });
+});
