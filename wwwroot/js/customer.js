50878e21c7ee8944c5e027e9bb1e9443e5261683
Jasmine Randhawa <jasminerandhawa05@gmail.com>
2021-06-10 02:47:08 -0600

    api call using ajax for customer and movie form

---
+++ wwwroot/js/customer.js
@@ -0,0 +1,15 @@
+﻿
+    $(document).ready(function () {
+        $("#customer").on("click", ".js-delete", function () {
+            var deleteButton = $(this);
+            if (confirm("Are you sure you want to delete this customer?")) {
+                $.ajax(
+                    {
+                        url: "/customer/delete/" + deleteButton.attr("data-customer-id"),
+                        success: function () {
+                            deleteButton.parents("tr").remove();
+                        }
+                    });
+            }
+        })
+    });
---
+++ wwwroot/js/movie.js
@@ -0,0 +1,15 @@
+﻿
+    $(document).ready(function () {
+        $("#movie").on("click", ".js-delete", function () {
+            var deleteButton = $(this);
+            if (confirm("Are you sure you want to delete this movie?")) {
+                $.ajax(
+                    {
+                        url: "/movie/delete/" + deleteButton.attr("data-movie-id"),
+                        success: function () {
+                            deleteButton.parents("tr").remove();
+                        }
+                    });
+            }
+        })
+    });
