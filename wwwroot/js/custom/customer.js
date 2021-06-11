ccb3e29d4aa8e1bbe947e1abf4a8c231b3106f35
Jasmine Randhawa <jasminerandhawa05@gmail.com>
2021-06-10 22:02:34 -0600

    configured datatables

---
+++ wwwroot/js/custom/customer.js
@@ -0,0 +1,19 @@
+﻿
+$(document).ready(function () {
+    $("#customer").DataTable();
+    $("#customer").on("click", ".js-delete", function () {
+       var deleteButton = $(this);
+        bootbox.confirm("Are you sure you want to delete this customer?", function (result)
+        {
+           if (result) {
+               $.ajax
+                   ({
+                       url: "/customer/delete/" + deleteButton.attr("data-customer-id"),
+                       success: function () {
+                           deleteButton.parents("tr").remove();
+                       }
+                   });
+           }
+        });
+      });
+});
---
+++ wwwroot/js/custom/movie.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $("#movie").DataTable();
+    $("#movie").on("click", ".js-delete", function () {
+        var deleteButton = $(this);
+        bootbox.confirm("Are you sure you want to delete this movie?", function (result) {
+            if (result) {
+                $.ajax
+                    ({
+                        url: "/movie/delete/" + deleteButton.attr("data-movie-id"),
+                        success: function () {
+                            deleteButton.parents("tr").remove();
+                        }
+
+                    });
+            }
+            else {
+
+            }
+         });
+    });
+});
